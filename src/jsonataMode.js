9470a97969167437680776c27a72aa08e5337a81
Chris Bygrave <chris.bygrave@kaleido.io>
2024-02-01 17:07:27 +0000

    Lazy string matching (#34)

--- src/jsonataMode.js
+++ src/jsonataMode.js
@@ -13,8 +13,8 @@ function registerJsonata(monaco) {
         tokenizer: {
             root: [
                 [/\/\*.*\*\//, "jsonata-comment"],
-                [/'.*'/, "jsonata-string"],
-                [/".*"/, "jsonata-string"],
+                [/'.*?'/, "jsonata-string"],
+                [/".*?"/, "jsonata-string"],
                 [/\$[a-zA-Z0-9_]*/, "jsonata-variable"],
                 [/[a-zA-Z0-9_]+/, "jsonata-names"],
             ]
